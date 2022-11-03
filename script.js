4c8058090a1e8e7498ee2691c13a6bbc380e5d34
Baba340 <115713984+Baba340@users.noreply.github.com>
2022-11-04 00:40:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ window.onload = function() {
     loading.animate([
       {opacity: 0},
       {opacity: 1}
-    ],1500);
+    ],2000);
     setTimeout(function() {
       setTimeout(function() {
 　　　　　//第四画面
