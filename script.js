a2d447c41be0ae6b1f237e2428fec067260f186e
Baba340 <115713984+Baba340@users.noreply.github.com>
2022-11-13 00:29:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ let rateViewBtn = document.getElementById("rateViewBtn");
 let ctxPlusBtn = document.getElementById("ctxPlusBtn");
 let ctxMinusBtn = document.getElementById("ctxMinusBtn");
 let backTitleBtn = document.getElementById("backTitleBtn");
+let changeNameBtn = document.getElementById("changeNameBtn");
 let questionNin = document.getElementById("questionNin");
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
@@ -45,6 +46,7 @@ exitBtn.style.display ="none";
 rateViewBtn.style.display ="none";
 ctxPlusBtn.style.display ="none";
 ctxMinusBtn.style.display ="none";
+changeNameBtn.style.display ="none";
 questionNin.style.display ="none";
 btn1.style.display ="none";
 btn2.style.display ="none";
@@ -61,6 +63,7 @@ let questionN=1;
 let wordsRandom = "";
 let wordsJpRandom = "";
 let wordsEnRandom = "";
+let name  = localStorage.getItem("name"); 
 let rate  = localStorage.getItem("rate"); 
 let titles = "神格者　THE LEAPer";
 let inputed = [];
@@ -74,7 +77,7 @@ let abcs = [...'abcdefghijklmnopqrstuvwxyz'];
 //問題
 var wordsBef =[
 //No.questionStartNum〜
-["【名】 （of ～）十分な～，たくさんの～","plenty"],["【形】 十分な","sufficient"],["【形】 多くの","numerous"],["【形】 ①かなりの，たくさんの ②内容のある，重要な","substantial"],["【名】 単位","unit"],["【名】 直径","diameter"],["【名】 量","quantity"],["【名】 欠乏，不足","deficiency"],["【他】 ～を減らす","lessen"],["【名】 時代","era"],["【名】 ①（活動の）期間 ②（議会などの）会期 ③（ある活動の）集まり","session"],["【名】 ①休止 [自] ②休止する","pause"],["【副】 ①その間 ②（対比を示して）その一方で","meanwhile"],["【名】 土産，記念品","souvenir"],["【名】 ①れんが ②〈英〉（おもちゃの）積み木","brick"],["【名】 ①棒，さお，柱 ②（天体，地球の）極","pole"],["【名】 ①糸 [他] ②～に糸を通す","thread"],["【名】 いたずら〈不可算〉","mischief"],["【名】 迷信","superstition"],["【名】 貯蔵，保管〈不可算〉","storage"],["【名】 ①避難（所） ②住居","shelter"],["【名】 衣類","garment"],["【名】 ①戸棚 ②（the C－）内閣","cabinet"],["【名】 ①レシピ，調理法 ②秘訣，原因","recipe"],["【名】 パンフレット","brochure"],["【名】 （商品の）発送","shipping"],["【他】 （主に〈英〉）～を取って来る","fetch"],["【他】 ①～を分配する，配る ②（be －d）分布している","distribute"],["【名】 像","statue"],["【名】 彫刻","sculpture"],["【他】 ～を彫る，刻む","carve"],["【名】 ①（俗説という意味での）神話 ②（古代の）神話","myth"],["【名】 ①工芸（品） ②（職人の）技術 ③船，乗り物","craft"],["【名】 ①雑誌，専門誌 ②（公的な）日誌，日記","journal"],["【名】 ①天国 ②（the －s）空","heaven"],["【名】 葬式","funeral"],["【名】 ①魂 ②（慣用句あるいは否定文で）人","soul"],["【名】 神父，僧侶，牧師，聖職者","priest"],["【名】 ①（主に宗教上の）信仰 ②信頼","faith"],["【形】 神聖な","sacred"],["【形】 神の","divine"],["【名】 ①文脈 ②状況","context"],["【形】 ①単数の ②（褒めて）比類なき ③奇妙な","singular"],["【名】 ①言語を操る力 ②命令 [他] ③～を集める ④～を見渡せる","command"],["【名】 例","instance"],["【名】 語り，物語","narrative"],["【名】 ①（本，劇，映画の中の）会話 ②対談，対話","dialogue"],["【名】 語法","usage"],["【名】 ①訛 ②アクセント","accent"],["【名】 ①気性 ②平静（な気分）","temper"],["【名】 愛情","affection"],["【名】 情熱，熱意","enthusiasm"],["【名】 情熱","passion"],["【名】 ①激怒 [自] ②（戦争，病気が）激しく続く，猛威をふるう","rage"],["【名】 悲しみ","sorrow"],["【形】 信じられない","incredible"],["【自】 ①くすくす笑う [名] ②くすくす笑うこと","chuckle"],["【他】 ～をからかう，冷やかす","tease"],["【他】 ①（to do）思い切って～する [助] ②（do）思い切って～する","dare"],["【形】 （be － to do）嫌がらずに～する","willing"],["【自】 ①悲鳴を上げる [名] ②悲鳴","scream"],["【他】 ①（人）をなだめる ②（痛みなど）をやわらげる","soothe"],["【他】 ～を誘惑する","tempt"],["【他】 ～を??る","scold"],["【他】 ～を圧迫する，虐げる","oppress"],["【他】 ①（to do）～すると脅す ②～を脅かす ③～のおそれがある","threaten"],["【他】 ①～を喜ばせる [名] ②（大）喜び","delight"],["【他】 ～を（とても）驚かせる","astonish"],["【他】 ～を嘆く，悼む","mourn"],["【他】 （人，物が）～を怒らせる，～の気分を害する","offend"],["【他】 ～を憂うつにさせる，落ち込ませる","depress"],["【他】 ①～を犠牲にする [名] ②犠牲","sacrifice"],["【名】 衝動","impulse"],["【名】 ①災いの元 ②呪い [他] ③～を呪う，ののしる","curse"],["【名】 要因","factor"],["【自】 ①（from ～）（～が）原因である [名] ②茎，幹，ワイングラスの脚","stem"],["【他】 ①～のきっかけになる，～を誘発する [名] ②（銃の）引き金","trigger"],["【名】 ①結果 ②（of －） 重要性","consequence"],["【名】 ①過程 [他] ②～を加工する，処理する","process"],["【自】 ①（to ～）（～に）訴える [名] ②行楽地 ③手段","resort"],["【名】 ①方法 ②秘けつ ③（数学，化学などの）式，公式","formula"],["【名】 手続き，手順","procedure"],["【形】 ①並外れた ②臨時の（会議など） ③特命の（大使など）","extraordinary"],["【形】 絶対的な","absolute"],["【形】 圧倒的な","overwhelming"],["【副】 ①完全に ②全部で","altogether"],["【形】 すべての","entire"],["【形】 すさまじい","tremendous"],["【形】 ①（数量やサイズを強調して）とてつもない～の ②まったくの","sheer"],["【形】 ①部分的な ②不公平な ③（to ～）（～が）大好きで","partial"],["【副】 ①かろうじて ②ほとんど～ない","barely"],["【形】 わずかな","slight"],["【副】 （数量が）おおよそ，約","approximately"],["【副】 頻繁に","frequently"],["【副】 ①どうやら～らしい ②見たところでは","apparently"],["【副】 ①（主に名詞の前で）～にすぎない ②（動詞などの前で）単に","merely"],["【副】 それにもかかわらず","nevertheless"],["【副】 ①何らかの方法で ②何らかの理由で，どういうわけか","somehow"],["【副】 ①それゆえ（に） ②このように","thus"],["【副】 ①まあまあ [形] ②かわいい","pretty"],["【前】 ～にもかかわらず","despite"]
+["【名】 （of ～）十分な～，たくさんの～","plenty"],["【形】 十分な","sufficient"],["【形】 多くの","numerous"],["【形】 ①かなりの，たくさんの ②内容のある，重要な","substantial"],["【名】 単位","unit"],["【名】 直径","diameter"],["【名】 量","quantity"],["【名】 欠乏，不足","deficiency"],["【他】 ～を減らす","lessen"],["【名】 時代","era"],["【名】 ①（活動の）期間 ②（議会などの）会期 ③（ある活動の）集まり","session"],["【名】 ①休止 [自] ②休止する","pause"],["【副】 ①その間 ②（対比を示して）その一方で","meanwhile"],["【名】 土産，記念品","souvenir"],["【名】 ①れんが ②〈英〉（おもちゃの）積み木","brick"],["【名】 ①棒，さお，柱 ②（天体，地球の）極","pole"],["【名】 ①糸 [他] ②～に糸を通す","thread"],["【名】 いたずら〈不可算〉","mischief"],["【名】 迷信","superstition"],["【名】 貯蔵，保管〈不可算〉","storage"],["【名】 ①避難（所） ②住居","shelter"],["【名】 衣類","garment"],["【名】 ①戸棚 ②（the C－）内閣","cabinet"],["【名】 ①レシピ，調理法 ②秘訣，原因","recipe"],["【名】 パンフレット","brochure"],["【名】 （商品の）発送","shipping"],["【他】 （主に〈英〉）～を取って来る","fetch"],["【他】 ①～を分配する，配る ②（be －d）分布している","distribute"],["【名】 像","statue"],["【名】 彫刻","sculpture"],["【他】 ～を彫る，刻む","carve"],["【名】 ①（俗説という意味での）神話 ②（古代の）神話","myth"],["【名】 ①工芸（品） ②（職人の）技術 ③船，乗り物","craft"],["【名】 ①雑誌，専門誌 ②（公的な）日誌，日記","journal"],["【名】 ①天国 ②（the －s）空","heaven"],["【名】 葬式","funeral"],["【名】 ①魂 ②（慣用句あるいは否定文で）人","soul"],["【名】 神父，僧侶，牧師，聖職者","priest"],["【名】 ①（主に宗教上の）信仰 ②信頼","faith"],["【形】 神聖な","sacred"],["【形】 神の","divine"],["【名】 ①文脈 ②状況","context"],["【形】 ①単数の ②（褒めて）比類なき ③奇妙な","singular"],["【名】 ①言語を操る力 ②命令 [他] ③～を集める ④～を見渡せる","command"],["【名】 例","instance"],["【名】 語り，物語","narrative"],["【名】 ①（本，劇，映画の中の）会話 ②対談，対話","dialogue"],["【名】 語法","usage"],["【名】 ①訛 ②アクセント","accent"],["【名】 ①気性 ②平静（な気分）","temper"],["【名】 愛情","affection"],["【名】 情熱，熱意","enthusiasm"],["【名】 情熱","passion"],["【名】 ①激怒 [自] ②（戦争，病気が）激しく続く，猛威をふるう","rage"],["【名】 悲しみ","sorrow"],["【形】 信じられない","incredible"],["【自】 ①くすくす笑う [名] ②くすくす笑うこと","chuckle"],["【他】 ～をからかう，冷やかす","tease"],["【他】 ①（to do）思い切って～する [助] ②（do）思い切って～する","dare"],["【形】 （be － to do）嫌がらずに～する","willing"],["【自】 ①悲鳴を上げる [名] ②悲鳴","scream"],["【他】 ①（人）をなだめる ②（痛みなど）をやわらげる","soothe"],["【他】 ～を誘惑する","tempt"],["【他】 ～をしかる","scold"],["【他】 ～を圧迫する，虐げる","oppress"],["【他】 ①（to do）～すると脅す ②～を脅かす ③～のおそれがある","threaten"],["【他】 ①～を喜ばせる [名] ②（大）喜び","delight"],["【他】 ～を（とても）驚かせる","astonish"],["【他】 ～を嘆く，悼む","mourn"],["【他】 （人，物が）～を怒らせる，～の気分を害する","offend"],["【他】 ～を憂うつにさせる，落ち込ませる","depress"],["【他】 ①～を犠牲にする [名] ②犠牲","sacrifice"],["【名】 衝動","impulse"],["【名】 ①災いの元 ②呪い [他] ③～を呪う，ののしる","curse"],["【名】 要因","factor"],["【自】 ①（from ～）（～が）原因である [名] ②茎，幹，ワイングラスの脚","stem"],["【他】 ①～のきっかけになる，～を誘発する [名] ②（銃の）引き金","trigger"],["【名】 ①結果 ②（of －） 重要性","consequence"],["【名】 ①過程 [他] ②～を加工する，処理する","process"],["【自】 ①（to ～）（～に）訴える [名] ②行楽地 ③手段","resort"],["【名】 ①方法 ②秘けつ ③（数学，化学などの）式，公式","formula"],["【名】 手続き，手順","procedure"],["【形】 ①並外れた ②臨時の（会議など） ③特命の（大使など）","extraordinary"],["【形】 絶対的な","absolute"],["【形】 圧倒的な","overwhelming"],["【副】 ①完全に ②全部で","altogether"],["【形】 すべての","entire"],["【形】 すさまじい","tremendous"],["【形】 ①（数量やサイズを強調して）とてつもない～の ②まったくの","sheer"],["【形】 ①部分的な ②不公平な ③（to ～）（～が）大好きで","partial"],["【副】 ①かろうじて ②ほとんど～ない","barely"],["【形】 わずかな","slight"],["【副】 （数量が）おおよそ，約","approximately"],["【副】 頻繁に","frequently"],["【副】 ①どうやら～らしい ②見たところでは","apparently"],["【副】 ①（主に名詞の前で）～にすぎない ②（動詞などの前で）単に","merely"],["【副】 それにもかかわらず","nevertheless"],["【副】 ①何らかの方法で ②何らかの理由で，どういうわけか","somehow"],["【副】 ①それゆえ（に） ②このように","thus"],["【副】 ①まあまあ [形] ②かわいい","pretty"],["【前】 ～にもかかわらず","despite"]
 ];
 
 //解説
@@ -86,7 +89,7 @@ var exps = {
 
 //アラート
 if(localStorage.getItem("mes1") == null || localStorage.getItem("mes1") == 0){
-    alert("画面上の+,-ボタンで画面サイズを機種に対応させてください。");
+    alert("画面上部の+,-ボタンで画面サイズを機種に対応させてください。\n問題は、指定された範囲でランダム出題されます。\nレートは強制終了されても反映されます。\nバク等あれば、製作者に報告してください。");
     localStorage.setItem("mes1",1);
   }
 
@@ -118,6 +121,7 @@ window.onload = function() {
         rateViewBtn.style.display ="block";
         ctxPlusBtn.style.display ="block";
         ctxMinusBtn.style.display ="block";
+        changeNameBtn.style.display ="block";
         banar();
       }, 1000);
     }, 1000);
@@ -444,7 +448,7 @@ function arrow(TF){
 
 //レート表示画面遷移
 function rateView(){
-  location.href = 'rates.html?rate=' +  encodeURIComponent(rate);
+  location.href = 'rates.html?rate=' +  encodeURIComponent(rate) + "?name=" +  encodeURIComponent(localStorage.getItem("name"));
 }
 
 //バナー描画
@@ -476,6 +480,29 @@ function banar(){
   ctx.fillText("🏆"+ String(localStorage.getItem("rate")),30,320);
   ctx.fillStyle = "white";
   ctx.fillText(titles,30,380);
+  ctx.font = "80px sans-serif";
+  ctx.textAlign = "left";
+  ctx.fillStyle = "white";
+  ctx.fillText(localStorage.getItem("name"),30,230);
+}
+
+//名前変更
+function changeName(){
+  let textLengthCount = 0;
+  let gotNameArray = [...window.prompt("ユーザー名を入力してください(全角5文字・半角10文字以内)",localStorage.getItem("name"))];
+  for(let i = 0; i < gotNameArray.length; i++){
+    if (gotNameArray[i].match(/^[^\x01-\x7E\xA1-\xDF]+$/)) {
+      textLengthCount += 2;
+    } else {
+      textLengthCount += 1;
+    }
+  }
+  if(textLengthCount <= 10){
+    localStorage.setItem("name",gotNameArray.join(""));
+    banar();
+  }else{
+    alert("ユーザー名は全角5文字・半角10文字以内で入力してください。");
+  }
 }
 
 //称号算出
